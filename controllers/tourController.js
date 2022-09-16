e4d2956e07a626912b90824ae378334a49125b9f
tuanna5 <tuanna5@smartosc.com>
2022-09-16 10:41:58 +0700

    Add some file

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,53 @@
 const Tour = require('./../models/tourModel');
 
+const apiFeatures = async (queryValue) => {
+  const queryObj = { ...queryValue };
+  const excludedFields = ['page', 'sort', 'limit', 'fields'];
+  excludedFields.forEach((element) => {
+    delete queryObj[element];
+  });
+
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (value) => `$${value}`);
+
+  let query = Tour.find(JSON.parse(queryStr));
+
+  if (queryValue.sort) {
+    const sortBy = queryValue.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  if (queryValue.fields) {
+    const fields = queryValue.fields.split(',').join(' ');
+    query = query.select(fields);
+  } else {
+    query = query.select('-__v');
+  }
+
+  if (queryValue.page && queryValue.limit) {
+    const totalTours = await Tour.countDocuments();
+    const page = +queryValue.page;
+    const limit = +queryValue.limit;
+    const skip = (page - 1) * limit;
+
+    if (skip >= totalTours) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'This page does not exist',
+      });
+    }
+
+    query = query.skip(skip).limit(limit);
+  }
+
+  return query;
+};
+
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
+    return res.status(404).json({
       status: 'error',
       message: 'Missing name or price',
     });
@@ -10,48 +55,16 @@ exports.checkBody = (req, res, next) => {
   next();
 };
 
+exports.topFiveCheap = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((element) => {
-      delete queryObj[element];
-    });
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (value) => `$${value}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    const page = +req.query.page || 1;
-    const limit = +req.query.limit || 1;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const totalTours = await Tour.countDocuments();
-      if (skip >= totalTours) {
-        return res.status(400).json({
-          status: 'error',
-          message: 'This page does not exist',
-        });
-      }
-    }
-
+    const query = apiFeatures(req.query);
     const tours = await query;
 
     res.status(201).json({
@@ -62,7 +75,7 @@ exports.getAllTours = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(400).json({
+    res.status(404).json({
       status: 'error',
       message: error,
     });
@@ -79,7 +92,7 @@ exports.createTour = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(400).json({
+    res.status(404).json({
       status: 'error',
       message: error,
     });
@@ -96,7 +109,7 @@ exports.getTour = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(400).json({
+    res.status(404).json({
       status: 'error',
       message: error,
     });
@@ -116,7 +129,7 @@ exports.updateTour = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(400).json({
+    res.status(404).json({
       status: 'error',
       message: error,
     });
@@ -130,13 +143,49 @@ exports.deleteTour = async (req, res) => {
       status: 'success',
     });
   } catch (error) {
-    res.status(400).json({
+    res.status(404).json({
       status: 'error',
       message: error,
     });
   }
 };
+
 exports.checkId = (req, res, next, val) => {
   console.log(`Tour id: ${val}`);
   next();
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: '$difficulty',
+          numTours: { $sum: 1 },
+          numRating: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
+
+    console.log(stats);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'error',
+      message: error,
+    });
+  }
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,8 @@ const tourController = require("./../controllers/tourController")
 const router = express.Router();
 
 router.param("id", tourController.checkId);
+router.route("/top-5-cheap").get(tourController.topFiveCheap, tourController.getAllTours)
+router.route("/tour-stats").get(tourController.getTourStats)
 router.route("/").get(tourController.getAllTours).post(tourController.checkBody, tourController.createTour);
 router.route("/:id").get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
